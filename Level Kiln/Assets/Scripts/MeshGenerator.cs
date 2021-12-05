14ba01be0b68e8a5520bf2d96f245a8de770d3af
Foremanski <daniel.foreman@students.plymouth.ac.uk>
2021-12-05 16:46:58 +0000

    Created Cube and Updated Readme
    Turned mesh into cube, and Updated Readme with resource
    
    Started work on resizing mesh

--- Level Kiln/Assets/Scripts/MeshGenerator.cs
+++ Level Kiln/Assets/Scripts/MeshGenerator.cs
@@ -21,12 +21,16 @@ public class MeshGenerator : MonoBehaviour
     {
         if(Input.GetKeyDown("e"))
         {
-            initialiseMesh();
+            InitialiseMesh();
+        }
+        if(Input.GetKeyDown("q"))
+        {
+            ResizeMesh();
         }
     }
 
 
-    void initialiseMesh()
+    void InitialiseMesh()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
@@ -40,13 +44,29 @@ public class MeshGenerator : MonoBehaviour
         vertices = new Vector3[]
         {
             new Vector3 (0,0,0),
-            new Vector3 (0,0,1),
-            new Vector3 (1,0,0)
+            new Vector3 (1,0,0),
+            new Vector3 (1,1,0),
+            new Vector3 (0,1,0),
+            new Vector3 (0,1,1),
+            new Vector3 (1,1,1),
+            new Vector3 (1,0,1),
+            new Vector3 (0,0,1)
         };
 
         triangles = new int[]
         {
-            0, 1, 2
+            0, 2, 1, //face front
+	        0, 3, 2,
+            2, 3, 4, //face top
+	        2, 4, 5,
+            1, 2, 5, //face right
+	        1, 5, 6,
+            0, 7, 4, //face left
+	        0, 4, 3,
+            5, 4, 7, //face back
+	        5, 7, 6,
+            0, 6, 7, //face bottom
+	        0, 1, 6
         };
     }
     
@@ -56,5 +76,14 @@ public class MeshGenerator : MonoBehaviour
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+    }
+
+    void ResizeMesh()
+    {
+        Mesh meshClone = mesh;
+
+        meshClone.vertices =
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # COMP3000-Project
 Repository for my COMP3000 project, currently named as LevelKiln. A 3d level design sketch tool that can be used for a wide range of uses
 
+Resources:
+https://www.raywenderlich.com/3169311-runtime-mesh-manipulation-with-unity
+
 Supervisor: Marius Varga
